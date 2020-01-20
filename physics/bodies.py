e3360734d4a5acc3a866fe174ddee53898d93c2b
lol-cubes <arinmkhare@gmail.com>
2020-01-20 15:42:43 -0500

    update Body object with mass, coords, and push method

--- physics/bodies.py
+++ physics/bodies.py
@@ -8,12 +8,17 @@ class Body:
     Velocity is a vector whose magnitude is in m/s
     """
 
-    def __init__(self, canvas, identifier, x, y, velocity=Vector(x=0, y=0)):
+    def __init__(self, canvas, identifier, x, y, velocity=Vector(x=0, y=0), mass=1):
         self.x = x
         self.y = y
         self.canvas = canvas
         self.id = identifier
         self.velocity = velocity
+        self.mass = mass
+
+    @property
+    def coords(self):
+        return self.x, self.y
 
     def move(self, t):
         """
@@ -25,3 +30,12 @@ class Body:
         self.canvas.move(self.id, horizontal_movement, vertical_movement)
         self.x += horizontal_movement
         self.y += vertical_movement
+
+    def push(self, f, t):
+        """
+        Applies work to self with force `f` for duration `t`
+        """
+        a = f * (1 / self.mass)  # acceleration
+        dv = a * t  # change in velocity
+        self.velocity += dv
+        self.move(t)
